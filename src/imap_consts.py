830b2c7fa7c93c7b769662f34573879c52d97424
grokas <grokas@splunk.com>
2026-03-25 16:11:17 -0700

    feat: v2 release

---
+++ src/imap_consts.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2016-2026 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+IMAP_CONNECTED_TO_SERVER = "Initiated connection to server"
+IMAP_ERROR_CONNECTING_TO_SERVER = "Error connecting to server"
+IMAP_ERROR_LISTING_FOLDERS = "Error listing folders"
+IMAP_ERROR_LOGGING_IN_TO_SERVER = "Error logging in to server"
+IMAP_ERROR_SELECTING_FOLDER = "Error selecting folder '{folder}'"
+IMAP_GOT_LIST_FOLDERS = "Got folder listing"
+IMAP_LOGGED_IN = "User logged in"
+IMAP_SELECTED_FOLDER = "Selected folder '{folder}'"
+IMAP_SUCCESS_CONNECTIVITY_TEST = "Connectivity test passed"
+IMAP_ERROR_CONNECTIVITY_TEST = "Connectivity test failed"
+IMAP_FETCH_ID_FAILED = "Fetch for uuid: {muuid} failed, reason: {excep}"
+IMAP_FETCH_ID_FAILED_RESULT = (
+    "Fetch for uuid: {muuid} failed, result: {result}, data: {data}"
+)
+IMAP_GENERAL_ERROR_MESSAGE = "{}. Details: {}"
